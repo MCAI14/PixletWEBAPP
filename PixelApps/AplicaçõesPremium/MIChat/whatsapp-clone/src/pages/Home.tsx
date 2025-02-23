384da929fed27843ae227b1a92321780d52a319e
MCAI14 <MCAI14@users.noreply.github.com>
2025-02-23 19:11:09 +0000

    PremiumApp is being created!

---
+++ PixelApps/AplicaçõesPremium/MIChat/whatsapp-clone/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Chat from '../components/Chat';
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to WhatsApp Clone</h1>
+            <Chat />
+        </div>
+    );
+};
+
+export default Home;
